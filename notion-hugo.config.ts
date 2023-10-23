6e77dfb1abe0cfc3a570db40deb8dd062c8e19b2
LiangSam <38411915+LiangSam@users.noreply.github.com>
2023-10-23 21:09:26 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://chandlerkak.notion.site/Notion-DoIt-4d017e1edac54d9ab419ee38ebeb65f3?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
